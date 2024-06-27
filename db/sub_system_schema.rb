bf4b2a9254449a140498471355119e26d66c99d3
koba-masa <kobayashi_mshr@koba-masa.com>
2024-06-27 22:24:01 +0900

    refactor: RuboCopの指摘対応

--- db/sub_system_schema.rb
+++ db/sub_system_schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,19 +13,18 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "castles", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 32, null: false
-    t.integer "prefecture", null: false
+  create_table 'castles', id: :serial, force: :cascade do |t|
+    t.string 'name', limit: 32, null: false
+    t.integer 'prefecture', null: false
   end
 
-  create_table "prefectures", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 32, null: false
-    t.string "name_en", limit: 32, null: false
+  create_table 'prefectures', id: :serial, force: :cascade do |t|
+    t.string 'name', limit: 32, null: false
+    t.string 'name_en', limit: 32, null: false
   end
 
-  add_foreign_key "castles", "prefectures", column: "prefecture", name: "castles_prefecture_fkey"
+  add_foreign_key 'castles', 'prefectures', column: 'prefecture', name: 'castles_prefecture_fkey'
 end
--- spec/routing/daimyos_route_spec.rb
+++ spec/routing/daimyos_route_spec.rb
@@ -4,6 +4,6 @@ require 'rails_helper'
 
 RSpec.describe 'routes for Daimyos' do
   it 'routes /daimyos to the daimyos controller' do
-    expect(get('/daimyos')).to route_to({"format"=>:json, "controller"=>"daimyos", "action"=>"index"})
+    expect(get('/daimyos')).to route_to({ 'format' => :json, 'controller' => 'daimyos', 'action' => 'index' })
   end
 end
