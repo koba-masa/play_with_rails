a4cd344d8530e0fc1d0efa394c3eee83edfd2660
koba-masa <kobayashi_mshr@koba-masa.com>
2025-02-24 15:47:11 +0900

    feat: 大名テーブルの名前に一意制約を追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_19_125310) do
+ActiveRecord::Schema.define(version: 2025_02_24_064000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_09_19_125310) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "kokushi", limit: 16, comment: "国司"
+    t.index ["name"], name: "index_daimyos_on_name", unique: true
   end
 
 end
